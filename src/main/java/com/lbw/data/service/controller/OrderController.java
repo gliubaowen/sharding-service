9d095e9add8aea748910dca694d80e25f0b05378
BaoWenLiu <BaoWenLiu@DESKTOP-CIITADI>
2019-11-10 02:31:49 +0800

    使用 spring boot yaml 方式完成第二版

--- src/main/java/com/lbw/data/service/controller/OrderController.java
+++ src/main/java/com/lbw/data/service/controller/OrderController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dangdang.ddframe.rdb.sharding.keygen.KeyGenerator;
 import com.lbw.data.service.entity.Order;
 import com.lbw.data.service.repository.OrderRepository;
 
@@ -18,18 +17,18 @@ public class OrderController {
 	@Autowired
 	private OrderRepository orderRepository;
 
-	@Autowired
-	private KeyGenerator keyGenerator;
+//	@Autowired
+//	private KeyGenerator keyGenerator;
 
 	@RequestMapping("/create")
 	public Object add() {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 1000; i++) {
 			Order order = new Order();
 			order.setUserId((long) i);
 			order.setOrderId((long) i);
 			orderRepository.save(order);
 		}
-		for (int i = 10; i < 20; i++) {
+		for (int i = 1000; i < 2000; i++) {
 			Order order = new Order();
 			order.setUserId((long) i + 1);
 			order.setOrderId((long) i);
@@ -50,15 +49,15 @@ public class OrderController {
 		return "success";
 	}
 
-	@RequestMapping("insert")
-	private Object insert() {
-		for (int i = 0; i < 30; i++) {
-			Order order = new Order();
-			order.setOrderId(keyGenerator.generateKey().longValue());
-			order.setUserId(keyGenerator.generateKey().longValue());
-			orderRepository.save(order);
-		}
-		return "success";
-	}
+//	@RequestMapping("insert")
+//	private Object insert() {
+//		for (int i = 0; i < 30; i++) {
+//			Order order = new Order();
+//			order.setOrderId(keyGenerator.generateKey().longValue());
+//			order.setUserId(keyGenerator.generateKey().longValue());
+//			orderRepository.save(order);
+//		}
+//		return "success";
+//	}
 
 }
